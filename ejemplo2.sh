6dfa1edb1f0028804fbf7f35974d27ea9c3f4da7
Rafa Laguna (ENTI) <rafa.laguna@enti.cat>
2020-11-09 12:05:21 +0100

    Ejemplo de variables y practicando vim

--- ejemplo2.sh
+++ ejemplo2.sh
@@ -2,4 +2,9 @@ ls -la | head -n 2
 date
 
 cal
-echo ola k ase
+echo ola dfdsafdsafk ase
+
+cowsay ola k ase
+
+ping -c 3 google.es
+
---
+++ saludo.sh
@@ -0,0 +1,8 @@
+NOMBRE_VACA="Pepa"
+
+
+echo "Hola! ¿Quién eres?" | cowsay
+
+read NOMBRE
+
+echo "Hola $NOMBRE, soy $NOMBRE_VACA. ¿Qué tal?" | cowsay
